6bde179149f00b6f6ab9addb77dc8a627a6480b4
Anas-7 <mohdmmedanas@gmail.com>
2023-11-30 17:04:34 -0800

    chess move notation validation

--- chess/app/Main.hs
+++ chess/app/Main.hs
@@ -4,7 +4,7 @@ module Main where
 import Lens.Micro ((^.))
 import Types
 import TestData
-
+import Data.Char (isDigit)
 {-# LANGUAGE OverloadedStrings #-}
 import Brick
 import Brick.Widgets.Border (border,hBorder ,vBorder)
@@ -54,6 +54,25 @@ safeBack :: [a] -> [a]
 safeBack [] = []
 safeBack xs = init xs
 
+isValidChessMove :: String -> Bool
+isValidChessMove move
+  | move == "O-O" || move == "O-O-O" = True  -- Castling
+  | otherwise = case move of
+      [file, rank] -> isFile file && isRank rank  -- Pawn move
+      [piece, file, rank] -> isPiece piece && isFile file && isRank rank
+      [file1, 'x', file2, rank] -> isFile file1 && isFile file2 && isRank rank  -- Pawn capture
+      [piece, file, 'x', file2, rank] -> isPiece piece && isFile file && isFile file2 && isRank rank
+      [piece, file1, rank1, file2, rank2] -> isPiece piece && isFile file1 && isRank rank1 && isFile file2 && isRank rank2  -- Disambiguating move
+      [file, rank, '=', promPiece] -> isFile file && isRank rank && isPromotionPiece promPiece  -- Pawn promotion
+      _ -> False
+  where
+    isFile f = f `elem` ['a'..'h']
+    isRank r = r `elem` ['1'..'8']
+    isPiece p = p `elem` "KQRBN"
+    isPromotionPiece p = p `elem` "QRBN"
+
+
+
 -- The app definition
 app :: App GameState e ()
 app =
@@ -69,20 +88,27 @@ app =
           VtyEvent (V.EvKey V.KEsc []) -> halt gs
           VtyEvent (V.EvKey (V.KChar c) []) -> continue $ gs { userInput = userInput gs ++ [c] }
           VtyEvent (V.EvKey V.KEnter []) -> do
+              -- Check if the move itself is valid
+              let validMove = isValidChessMove (userInput gs)
+              liftIO $ putStrLn $ if validMove then "Valid move" else "Invalid move"
               liftIO $ putStrLn ("User input: " ++ userInput gs)  -- Debugging output
-              -- Switch the player
-              let newPlayer = if currentPlayer gs == White then Black else White
-              -- Add the new player to the game state
-              continue $ gs {currentPlayer = newPlayer,  userInput = "" }  -- Clear input after enter
+              -- Implement the logic to actually change the board state if the move is valid
+              
+              -- Change player only if the move is valid
+              let newGameState = if validMove 
+                          then gs {currentPlayer = if currentPlayer gs == White then Black else White, userInput = "" } 
+                            else gs { userInput = "" }
+              continue newGameState
+
           VtyEvent (V.EvKey V.KBS []) -> continue $ gs { userInput = safeBack (userInput gs) }
           VtyEvent (V.EvKey (V.KChar 'm') []) -> captureMove gs -- 'm' for move, this is a placeholder
           _ -> continue gs
       , appStartEvent = return
       , appAttrMap = const $ attrMap V.defAttr [ (attrName "blackPiece", V.white `on` V.black)
-  , (attrName "whitePiece", V.black `on` V.white)
-  , (attrName "lightSquare", (V.rgbColor 220 220 220) `on` V.white) -- Cant directly use V.rgbColor in attrMap
-  , (attrName "darkSquare", V.black `on` V.green)
-  ]
+                                                , (attrName "whitePiece", V.black `on` V.white)
+                                                , (attrName "lightSquare", (V.rgbColor 220 220 220) `on` V.white) -- Cant directly use V.rgbColor in attrMap
+                                                , (attrName "darkSquare", V.black `on` V.green)
+                                                ]
       }
 captureMove :: GameState -> EventM n (Next GameState)
 captureMove gs = do
