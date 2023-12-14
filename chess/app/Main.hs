62e9aed6b5279456a59ecc588aaed24be2962034
Nimit Vasavat <nimit@Nimits-MacBook-Air.local>
2023-12-13 19:07:58 -0800

    parse error

--- chess/app/Main.hs
+++ chess/app/Main.hs
@@ -93,7 +93,7 @@ appEvent (VtyEvent e) = do
           V.EvKey V.KEsc [] -> halt
           V.EvKey (V.KChar c) [] -> do
               let newInput = userInput gs ++ [c]
-              let parsedInput = if length newInput > 1 &&
+              let parsedInput = if length newInput > 1 && isDigit (newInput !! 1) &&
                         compareColorPlayer (getBoardPieceColor (board gs) (rankToIndex $ newInput !! 1, fileToIndex $ newInput !! 0)) (currentPlayer gs)
                     then Just (fileToIndex $ newInput !! 0, rankToIndex $ newInput !! 1)
                     else Nothing
